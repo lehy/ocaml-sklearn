535019e1ef07c829b1f897510525c0362c300a20
Ronan Le Hy <ronan.lehy@gmail.com>
2020-04-02 17:46:40 +0200

    added SVC test

--- examples/auto/svm.ml
+++ examples/auto/svm.ml
@@ -126,23 +126,22 @@ SVC(gamma='auto')
 
 *)
 
-(* TEST TODO
 let%expect_test "SVC" =
-    import numpy as np    
-    X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])    
-    y = np.array([1, 1, 2, 2])    
-    let svc = Sklearn.Svm.svc in
-    clf = SVC(gamma='auto')    
-    print @@ fit clf x y
-    [%expect {|
-            SVC(gamma='auto')            
+  let open Sklearn.Svm in
+  let x = matrix [|[|-1.; -1.|]; [|-2.; -1.|]; [|1.; 1.|]; [|2.; 1.|]|] in
+  let y = vector [|1.; 1.; 2.; 2.|] in
+  let clf = SVC.create ~gamma:`Auto () in    
+  print SVC.pp @@ SVC.fit clf ~x ~y ();
+  [%expect {|
+            SVC(C=1.0, break_ties=False, cache_size=200, class_weight=None, coef0=0.0,
+                decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+                max_iter=-1, probability=False, random_state=None, shrinking=True,
+                tol=0.001, verbose=False)
+    |}];
+  print_ndarray @@ SVC.predict clf ~x:(matrix [|[|-0.8; -1.|]|]);
+  [%expect {|
+            [1.]
     |}]
-    print(clf.predict([[-0.8, -1]]))    
-    [%expect {|
-            [1]            
-    |}]
-
-*)
 
 
 
