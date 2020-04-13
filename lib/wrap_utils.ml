12c828a071d33f964545661f172ea665c5617aff
Ronan Le Hy <ronan.lehy@gmail.com>
2020-04-13 23:22:14 +0200

    add version check and support for generating nice docs

--- lib/wrap_utils.ml
+++ lib/wrap_utils.ml
@@ -16,9 +16,42 @@ module Option = struct
     | Some x -> Some (f x)
 end
 
+let runtime_sklearn_version () =
+  match Py.Object.get_attr_string (Py.import "sklearn") "__version__" with
+  | None -> raise Not_found
+  | Some version ->
+    let version = Py.String.to_string version in
+    let vlist = version |> String.split_on_char '.' |> List.map int_of_string in
+    match vlist with
+    | [major; minor; revision] -> Ok (major, minor, revision)
+    | _ -> Error version
+
+
+let do_check_version () =
+  let (wanted_major, wanted_minor) = Version.version in
+  match runtime_sklearn_version () with
+  | Ok (major, minor, _) ->
+    if (major, minor) = (wanted_major, wanted_minor) then `Ok
+    else `Version_mismatch (Printf.sprintf "wanted: %d.%d, running: %d.%d" wanted_major wanted_minor major minor)
+  | Error version -> `Cannot_determine_runtime_version (Printf.sprintf "cannot parse runtime sklearn version %s" version)
+
+let version_checked = ref false
+
+let check_version () =
+  if not !version_checked then begin
+    version_checked := true;
+    match do_check_version () with
+    | `Ok -> ()
+    | `Version_mismatch msg -> failwith msg
+    | `Cannot_determine_runtime_version msg ->
+      Printf.eprintf "ocaml-sklearn: warning: %s\n" msg
+  end
+
 let init () =
-  if not @@ Py.is_initialized () then
-    Py.initialize ()
+  if not @@ Py.is_initialized () then begin
+    Py.initialize ();
+  end;
+  check_version ();
 
 module Slice = struct
   type t = Py.Object.t
