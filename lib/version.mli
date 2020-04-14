b6e905f9dd9e3b1c8a3b8c4e9da5e9f64cdaf15a
Ronan Le Hy <ronan.lehy@gmail.com>
2020-04-14 17:02:34 +0200

    Add version.mli.

---
+++ lib/version.mli
@@ -0,0 +1,7 @@
+val full_version : int * int * int
+(** The full (major, minor, revision) sklearn version for which this
+   package was created. *)
+                   
+val version : int * int
+(** The (major, minor) sklearn version for which this package was
+   created. *)
