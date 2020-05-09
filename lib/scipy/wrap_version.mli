f9ffe56a5080a0f000cd43caf3e21f3b45728a5a
Ronan Le Hy <ronan.lehy@gmail.com>
2020-05-09 18:17:26 +0200

    Add wrap_version.mli.

---
+++ lib/scipy/wrap_version.mli
@@ -0,0 +1,9 @@
+(** ### full_version
+
+    The full sklearn version for which this package was created. *)
+val full_version : string list
+
+(** ### version
+
+    The (major, minor) sklearn version for which this package was created. *)
+val version : int * int
