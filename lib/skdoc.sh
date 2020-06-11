e4b96ddf88cff7a5a69fa28bcf302e077cd0bbe9
Ronan Le Hy <ronan.lehy@gmail.com>
2020-06-11 14:16:39 +0200

    skdoc.sh: install the right np, scipy versions and name .venv better

--- lib/skdoc.sh
+++ lib/skdoc.sh
@@ -3,7 +3,9 @@ set -e
 set -x
 
 ROOT=$(realpath $(dirname "$0"))/..
-VENV="$ROOT/.venv-$@"
+# Name the venvs differently to avoid concurrency problems when
+# running several tasks in parallel.
+VENV="$ROOT/.venv-$1-$2"
 echo "ROOT from $0 is $ROOT"
 source "$ROOT/lib/version.sh"
 if ! test -e "$VENV/bin/activate"; then
@@ -11,5 +13,5 @@ if ! test -e "$VENV/bin/activate"; then
    python3 -mvenv "$VENV"
 fi
 source "$VENV"/bin/activate
-pip install scikit-learn=="$SKLEARN_FULL_VERSION" pytest regex cffi
+pip install scikit-learn=="$SKLEARN_FULL_VERSION" numpy=="$NUMPY_FULL_VERSION" scipy=="$SCIPY_FULL_VERSION" pytest regex cffi
 python3 "$ROOT"/lib/skdoc.py "$@"
